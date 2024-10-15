27e6bba13f4eba7dfc1705c45bee64751976c877
Elias Zambrano <adrianzambranomontero@gmail.com>
2024-10-15 21:56:04 +0200

    register creado

--- app/src/main/java/com/aadrizeta/first/Login.java
+++ app/src/main/java/com/aadrizeta/first/Login.java
@@ -2,14 +2,21 @@ package com.aadrizeta.first;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Login extends AppCompatActivity {
+
+    private boolean isPasswordVisible = false; // Control de visibilidad de la contraseña
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,29 +24,57 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        // Referencias a las vistas en activity_login.xml
         Button loginButton = findViewById(R.id.PasswordButton);
         TextView registerTextView = findViewById(R.id.loginRegister);
 
+        TextInputLayout passwordInputLayout = findViewById(R.id.Password);
+        EditText passwordEditText = (EditText) passwordInputLayout.getEditText();
+
+        // Listener para el botón de iniciar sesión
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchMain();
             }
         });
+
+        // Listener para el texto de registro
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchRegister();
             }
         });
+
+        // Listener para mostrar/ocultar la contraseña al tocar el ícono del TextInputLayout
+        passwordInputLayout.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    assert passwordEditText != null;
+                    passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    passwordInputLayout.setEndIconDrawable(R.drawable.baseline_visibility_24);
+                } else {
+                    assert passwordEditText != null;
+                    passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    passwordInputLayout.setEndIconDrawable(R.drawable.baseline_visibility_off_24);
+                }
+                isPasswordVisible = !isPasswordVisible;
+            }
+        });
     }
-    public void launchMain(){
+
+    // Método para lanzar la actividad principal
+    public void launchMain() {
         Intent intent = new Intent(Login.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
-    public void launchRegister(){
+
+    // Método para lanzar la actividad de registro
+    public void launchRegister() {
         Intent intent = new Intent(Login.this, Register.class);
         startActivity(intent);
     }
--- app/src/main/java/com/aadrizeta/first/Register.java
+++ app/src/main/java/com/aadrizeta/first/Register.java
@@ -1,24 +1,85 @@
 package com.aadrizeta.first;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Register extends AppCompatActivity {
+
+    private boolean isPasswordVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button registerButton = findViewById(R.id.registerButton);
+
+        TextInputLayout passwordInputLayout = findViewById(R.id.passwordInput);
+        EditText passwordEditText = (EditText) passwordInputLayout.getEditText();
+
+        TextInputLayout passwordInputLayoutConfirmation = findViewById(R.id.passwordInputConfirmation);
+        EditText passwordEditTextConfirmation = (EditText) passwordInputLayoutConfirmation.getEditText();
+
+        //Mostrar contraseña caja1
+        passwordInputLayout.setEndIconOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible){
+                    assert passwordEditText != null;
+                    passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    passwordInputLayout.setEndIconDrawable(R.drawable.baseline_visibility_24);
+                } else {
+                    assert passwordEditText != null;
+                    passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    passwordInputLayout.setEndIconDrawable(R.drawable.baseline_visibility_off_24);
+                }
+                isPasswordVisible = !isPasswordVisible;
+            }
         });
+
+        //Mostrar contraseña caja2
+        passwordInputLayoutConfirmation.setEndIconOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible){
+                    assert passwordEditTextConfirmation != null;
+                    passwordEditTextConfirmation.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    passwordInputLayoutConfirmation.setEndIconDrawable(R.drawable.baseline_visibility_24);
+                } else {
+                    assert passwordEditTextConfirmation != null;
+                    passwordEditTextConfirmation.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    passwordInputLayoutConfirmation.setEndIconDrawable(R.drawable.baseline_visibility_off_24);
+                }
+                isPasswordVisible =!isPasswordVisible;
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              launchMain();
+          }
+        });
+
+
+    }
+    // Método para lanzar la actividad principal
+    public void launchMain() {
+        Intent intent = new Intent(Register.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
