630dfe6d92b53f8d302c46be029805a3c4f5eb95
unknown <adrianzambranomontero@gmail.com>
2024-10-15 10:00:02 +0200

    a

--- app/src/main/java/com/aadrizeta/first/Login.java
+++ app/src/main/java/com/aadrizeta/first/Login.java
@@ -2,35 +2,15 @@ package com.aadrizeta.first;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-import android.widget.ImageView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.aadrizeta.first.databinding.ActivityLoginBinding;
-
 public class Login extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        setSupportActionBar(binding.loginLogo);
-
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
     }
-
-    private void setSupportActionBar(ImageView loginLogo) {
-    }
-
 }
