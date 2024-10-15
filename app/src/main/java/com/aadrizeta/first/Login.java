d8d6ba55d4af1da938ae0073bdd6ce2c1026631d
unknown <adrianzambranomontero@gmail.com>
2024-10-15 11:20:46 +0200

    a

--- app/src/main/java/com/aadrizeta/first/Login.java
+++ app/src/main/java/com/aadrizeta/first/Login.java
@@ -1,6 +1,10 @@
 package com.aadrizeta.first;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +16,31 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.PasswordButton);
+        TextView registerTextView = findViewById(R.id.loginRegister);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchMain();
+            }
+        });
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchRegister();
+            }
+        });
+    }
+    public void launchMain(){
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    public void launchRegister(){
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
     }
 }
